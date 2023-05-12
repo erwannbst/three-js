09f813936dbe647f7dc105c4f5ce935b91f08d7f
Erwann <erwann.bst@gmail.com>
2023-05-12 09:20:38 +0200

    collision

--- src/my-three.ts
+++ src/my-three.ts
@@ -6,16 +6,18 @@ import {
   WebGLRenderer,
   sRGBEncoding,
 } from "three";
+import * as THREE from "three";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
 
 let camera: PerspectiveCamera;
 let scene: Scene;
 let renderer: WebGLRenderer;
-let bones: any = [];
+// let bones: any = [];
+const cubes: any = [];
 
 init();
 animate();
@@ -35,34 +37,43 @@ function init() {
     scene.background = texture;
     scene.environment = texture;
 
-    const loader = new GLTFLoader().setPath("/public/");
-    loader.load("tentacule.gltf", (gltf) => {
-      const tentacule = gltf.scene;
+    for (let i = 0; i < 30; i++) {
+      addCube(Math.random() * 10 - 5, Math.random() * 10 - 5, Math.random() * 10 - 5);
+    }
 
-      for (let i = 0; i < 8; i++) {
-        const tentaculeClone = clone(tentacule);
-        let bone = tentaculeClone.getObjectByName("Bone");
-        let currentBone = bone?.children;
-        bones[i] = [];
+    // const quaternion = new THREE.Quaternion();
+    // quaternion.setFromAxisAngle(new THREE.Vector3(2, 10, 3), Math.PI);
+    // const cube = addCube(10, 2, 2);
+    // cube.applyQuaternion(quaternion);
 
-        while (!(currentBone === undefined) && currentBone.length > 0) {
-          bones[i].push(currentBone[0]);
-          currentBone = currentBone[0].children;
-        }
+    // const loader = new GLTFLoader().setPath("/public/");
+    // loader.load("tentacule.gltf", (gltf) => {
+    //   const tentacule = gltf.scene;
 
-        // position the 8 tentacules around the main one
-        tentaculeClone.position.x = Math.cos((i * Math.PI) / 4) * 0.5;
-        tentaculeClone.position.y = Math.sin((i * Math.PI) / 4) * 0.5;
-        tentaculeClone.position.z = 0;
+    //   for (let i = 0; i < 8; i++) {
+    //     const tentaculeClone = clone(tentacule);
+    //     let bone = tentaculeClone.getObjectByName("Bone");
+    //     let currentBone = bone?.children;
+    //     bones[i] = [];
 
-        // rotate the 8 tentacules around the main one
-        tentaculeClone.rotation.z = (i * Math.PI) / 4;
+    //     while (!(currentBone === undefined) && currentBone.length > 0) {
+    //       bones[i].push(currentBone[0]);
+    //       currentBone = currentBone[0].children;
+    //     }
 
-        scene.add(tentaculeClone);
-      }
+    //     // position the 8 tentacules around the main one
+    //     tentaculeClone.position.x = Math.cos((i * Math.PI) / 4) * 0.5;
+    //     tentaculeClone.position.y = Math.sin((i * Math.PI) / 4) * 0.5;
+    //     tentaculeClone.position.z = 0;
 
-      // scene.add(tentacule);
-    });
+    //     // rotate the 8 tentacules around the main one
+    //     tentaculeClone.rotation.z = (i * Math.PI) / 4;
+
+    //     scene.add(tentaculeClone);
+    //   }
+
+    //   // scene.add(tentacule);
+    // });
 
     animate();
   });
@@ -99,15 +110,85 @@ function render() {
   renderer.render(scene, camera);
 }
 
+function addCube(px: number, py: number, pz: number) {
+  var colorandom = new THREE.Color(0xffffff);
+  colorandom.setHex(Math.random() * 0xffffff);
+  var geometry = new THREE.BoxGeometry(1, 1, 1); //x,y,z
+  var boxMaterial = new THREE.MeshBasicMaterial({ color: colorandom });
+  var cube = new THREE.Mesh(geometry, boxMaterial);
+
+  cube.position.set(px, py, pz);
+  cube.geometry.computeBoundingBox(); // null sinon
+  const direction = new THREE.Vector3(
+    Math.random() * 0.1 - 0.05,
+    Math.random() * 0.1 - 0.05,
+    Math.random() * 0.1 - 0.05
+  );
+  cubes.push({ cube, direction });
+  scene.add(cube);
+  return cube;
+}
+
 function animate() {
   requestAnimationFrame(animate);
 
-  bones.forEach((tent: any, tentNb: number) => {
-    const dephase = ((tentNb + 1) / 8 + 20) / 30;
-    tent.forEach((bone: any, index: number) => {
-      bone.rotation.x = -Math.abs((Math.sin(Date.now() * 0.002 * dephase) * 0.1 * index) / 3);
+  // make each cube move in a random direction
+  cubes.forEach(({ cube, direction }: any, index: number) => {
+    cube.position.x += direction.x * (index + 5) * 0.05;
+    cube.position.y += direction.y * (index + 5) * 0.05;
+    cube.position.z += direction.z * (index + 5) * 0.05;
+
+    cubes.forEach(({ cube: cube2 }: any, index2: number) => {
+      if (index !== index2) {
+        const box1 = new THREE.Box3().setFromObject(cube);
+        const box2 = new THREE.Box3().setFromObject(cube2);
+
+        if (box1.intersectsBox(box2)) {
+          console.log("collision");
+
+          // make the cube rebondir on the other cube according to the angle of collision
+          const angle = Math.atan2(
+            cube.position.y - cube2.position.y,
+            cube.position.x - cube2.position.x
+          );
+          const angle2 = Math.atan2(
+            cube.position.z - cube2.position.z,
+            cube.position.x - cube2.position.x
+          );
+          const angle3 = Math.atan2(
+            cube.position.y - cube2.position.y,
+            cube.position.z - cube2.position.z
+          );
+          cube.position.x = cube2.position.x + Math.cos(angle) * 1.1;
+          cube.position.y = cube2.position.y + Math.sin(angle2) * 1.1;
+          cube.position.z = cube2.position.z + Math.sin(angle3) * 1.1;
+        }
+      }
     });
+
+    // if the cube is out of the screen, put it back in the screen
+    if (
+      cube.position.x > 10 ||
+      cube.position.x < -10 ||
+      cube.position.y > 10 ||
+      cube.position.y < -10 ||
+      cube.position.z > 10 ||
+      cube.position.z < -10
+    ) {
+      console.log("out of screen", cube.position.x, cube.position.y, cube.position.z);
+      // make the cube stay in the screen
+      direction.x = -direction.x;
+      direction.y = -direction.y;
+      direction.z = -direction.z;
+    }
   });
+
+  // bones.forEach((tent: any, tentNb: number) => {
+  //   const dephase = ((tentNb + 1) / 8 + 20) / 30;
+  //   tent.forEach((bone: any, index: number) => {
+  //     bone.rotation.x = -Math.abs((Math.sin(Date.now() * 0.002 * dephase) * 0.1 * index) / 3);
+  //   });
+  // });
 
   render();
 }
